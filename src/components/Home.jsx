928b68b9f1279c1c67f042c22245979c40a09c52
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2025-03-10 21:01:19 +0600

    deploy

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <div></div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+};
+
+export default Home;
